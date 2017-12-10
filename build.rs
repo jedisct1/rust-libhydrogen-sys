96af0f84c6e6a8173dcc46d67ec0f9ad5053a81e
Frank Denis <github@pureftpd.org>
2017-12-10 02:45:08 +0100

    Initial import

---
+++ build.rs
@@ -0,0 +1,23 @@
+extern crate bindgen;
+extern crate gcc;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    gcc::Build::new().file("libhydrogen/hydrogen.c").compile(
+        "libhydrogen",
+    );
+
+    println!("cargo:rustc-link-lib=hydrogen");
+
+    let bindings = bindgen::Builder::default()
+        .header("libhydrogen/hydrogen.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_init() {
+        let res = unsafe { hydro_init() };
+        assert_eq!(res, 0);
+    }
+}
