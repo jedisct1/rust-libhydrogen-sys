aea3312ed1ce476ed1d4498cc49dd8cd0cb7f0bc
Frank Denis <github@pureftpd.org>
2021-11-01 13:30:10 +0100

    whitelist -> allowlist

--- build.rs
+++ build.rs
@@ -1,6 +1,3 @@
-use bindgen;
-use cc;
-
 use std::env;
 use std::path::PathBuf;
 
@@ -13,13 +10,13 @@ fn main() {
 
     let bindings = bindgen::Builder::default()
         .header("libhydrogen/hydrogen.h")
-        .whitelist_function("hydro_.*")
-        .whitelist_function("randombytes_.*")
-        .whitelist_type("hydro_.*")
-        .whitelist_type("randombytes_.*")
-        .whitelist_var("HYDRO_.*")
-        .whitelist_var("hydro_.*")
-        .whitelist_var("randombytes_.*")
+        .allowlist_function("hydro_.*")
+        .allowlist_function("randombytes_.*")
+        .allowlist_type("hydro_.*")
+        .allowlist_type("randombytes_.*")
+        .allowlist_var("HYDRO_.*")
+        .allowlist_var("hydro_.*")
+        .allowlist_var("randombytes_.*")
         .size_t_is_usize(true)
         .derive_copy(true)
         .derive_debug(true)
